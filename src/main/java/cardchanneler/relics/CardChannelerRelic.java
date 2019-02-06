19be2dae93a10dd2971a1ab3e2bf09bc3ea6c055
bug_sniper <sbluen153@gmail.com>
2019-02-05 22:28:31 -0800

    improve code style

--- src/main/java/cardchanneler/relics/CardChannelerRelic.java
+++ src/main/java/cardchanneler/relics/CardChannelerRelic.java
@@ -9,6 +9,9 @@ import org.apache.logging.log4j.Logger;
 import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.mod.stslib.relics.ClickableRelic;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.cards.blue.Strike_Blue;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
@@ -24,11 +27,12 @@ public class CardChannelerRelic  extends CustomRelic implements ClickableRelic {
     
     @Override
     public void onRightClick() {
+    	DamageType x = DamageInfo.DamageType.THORNS;
     	boolean outsideCombat = AbstractDungeon.getCurrRoom().phase != AbstractRoom.RoomPhase.COMBAT;
     			if (outsideCombat){
     				logger.info("You cannot use this relic outside combat");
     			}
-        final AbstractOrb orb = new ChanneledCard();
+        final AbstractOrb orb = new ChanneledCard(new Strike_Blue());
         AbstractDungeon.actionManager.addToBottom(new ChannelAction(orb));
     }
     
